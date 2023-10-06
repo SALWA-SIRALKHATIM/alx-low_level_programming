2f14afe1f77baf43f85057d14538ff4c3e9ce07d
SALWA-SIRALKHATIM <salwasiralkhatim@gmail.com>
2023-10-05 18:49:58 -0700

    hi

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,14 @@
 #include <stdio.h>
+/**
+ * print_art_sentence - Prints a sentence with proper grammar that results in a piece of art.
+ *
+ * Description:
+ * This function prints a sentence using the C programming language. The sentence is carefully
+ * crafted to have proper grammar and the outcome is intended to be a piece of art. The sentence
+ * is printed to the standard output, followed by a new line character.
+ *
+ * Return: void
+ */
 int main(void)
 
 {
