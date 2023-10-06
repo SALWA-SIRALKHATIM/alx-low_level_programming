8ce7b021c4540f0a3e824ad68923c8ac908b08cc
SALWA-SIRALKHATIM <salwasiralkhatim@gmail.com>
2023-10-05 18:34:13 -0700

    1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 /**
- * Print a text using puts function
- * main: theEntry point
- * return: always 0
+ * The program is to Print a text using puts function
+ * main: The entry point, starter of the program
+ * return: The intended text, and the program ends with 0
  */
 int main(void)
 {
