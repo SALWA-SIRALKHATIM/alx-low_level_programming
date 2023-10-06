e042e77e37a838f6baf45ca9154d02fda7dff587
SALWA-SIRALKHATIM <salwasiralkhatim@gmail.com>
2023-10-05 17:33:42 -0700

    5

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,15 +2,15 @@
 /**
  * main function
  * declaration: @intiger variable
- * 		@char variable
- * 		@long intiger variable
- * 		@ling long intiger variable
- * 		float vaiable
+ *		@char variable
+ *		@long intiger variable
+ *		@ling long intiger variable
+ *		float vaiable
  *
- * 		ziseof function: To compute the sizes of the types
- * 		return
+ *		ziseof function: To compute the sizes of the types
+ *	return
  */
-int main()
+int main(void)
 {
 	char charvar;
 	int intvar;
@@ -19,11 +19,11 @@ int main()
 	float floatvar;
 
 	/*Determination of the sizes of the various types on this computer*/
-	printf("Size of a char: %zu  byte(s)\n", sizeof(charvar));
+	printf("Size of a char: %zu byte(s)\n", sizeof(charvar));
 	printf("Size of an int: %zu byte(s)\n", sizeof(intvar));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(lintvar));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(llintvar));
 	printf("Size of a float: %zu byes(s)\n", sizeof(floatvar));
 
-	return(0);
+	return (0);
 }
