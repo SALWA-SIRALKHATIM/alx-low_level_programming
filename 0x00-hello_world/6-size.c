eb1b835b129f1316e36c0606d9d2764dca20bc7e
SALWA-SIRALKHATIM <salwasiralkhatim@gmail.com>
2023-10-05 17:44:46 -0700

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,29 +1,33 @@
 #include <stdio.h>
+
+
 /**
- * main function
- * declaration: @intiger variable
- *		@char variable
- *		@long intiger variable
- *		@ling long intiger variable
- *		float vaiable
+
+ * main - Prints numbers from 0 to 9.
+
  *
- *		ziseof function: To compute the sizes of the types
- *	return
+
+ * Return: Always 0 (Success)
+
  */
+
 int main(void)
+
 {
-	char charvar;
-	int intvar;
-	long int lintvar;
-	long long int llintvar;
-	float floatvar;
 
-	/*Determination of the sizes of the various types on this computer*/
-	printf("Size of a char: %zu byte(s)\n", sizeof(charvar));
-	printf("Size of an int: %zu byte(s)\n", sizeof(intvar));
-	printf("Size of a long int: %zu byte(s)\n", sizeof(lintvar));
-	printf("Size of a long long int: %zu byte(s)\n", sizeof(llintvar));
-	printf("Size of a float: %zu byes(s)\n", sizeof(floatvar));
+        int i;
 
-	return (0);
+
+        for (i = 48; i < 58; i++)
+
+        {
+
+                putchar(i);
+
+        }
+
+        putchar('\n');
+
+        return (0);
+
 }
