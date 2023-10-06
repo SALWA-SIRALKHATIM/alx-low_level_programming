326ba731486f6096d2cb93e04cecb251910fd6f9
SALWA-SIRALKHATIM <salwasiralkhatim@gmail.com>
2023-10-05 17:57:55 -0700

    f

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,8 @@
-#include <stdio.h>
-
-#include <stdlib.h>
-
-#include <string.h>
-
+#include<stdio.h>
 
 /**
 
- * main - Prints the alphabet without q and e.
+ * main - Entry point
 
  *
 
@@ -19,27 +14,8 @@ int main(void)
 
 {
 
-        int i;
-
-
-        for (i = 97; i < 123; i++)
-
-        {
-
-                if (i != 101 && i != 113)
-
-                {
-
-                        putchar(i);
-
-                }
-
-        }
-
-        putchar('\n');
+        puts("\"Programming is like building a multilingual puzzle");
 
         return (0);
 
 }
-
-
