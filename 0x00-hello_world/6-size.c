d67fe1cbacbebf7d7bd7b454c6cc2a16856e414f
SALWA-SIRALKHATIM <salwasiralkhatim@gmail.com>
2023-10-05 17:59:39 -0700

    m

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,32 +1,28 @@
-#include <stdio.h>
-
+#include<stdio.h>
 
 /**
 
- * main - Prints numbers from 0 to 9.
+* main - Entry point
 
- *
+*
 
- * Return: Always 0 (Success)
+* Return: Always 0 (Success)
 
- */
+*/
 
 int main(void)
 
 {
 
-        int i;
+        printf("Size of a char: %d byte(s)\n", sizeof(char));
 
+        printf("Size of an int: %d byte(s)\n", sizeof(int));
 
-        for (i = 48; i < 58; i++)
+        printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 
-        {
+        printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
 
-                putchar(i);
-
-        }
-
-        putchar('\n');
+        printf("Size of a float: %d byte(s)\n", sizeof(float));
 
         return (0);
 
