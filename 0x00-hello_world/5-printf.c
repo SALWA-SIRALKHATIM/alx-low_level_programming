c0aba4b8bbbc5146e498178acfd5e2eba3225dc6
SALWA-SIRALKHATIM <salwasiralkhatim@gmail.com>
2023-10-05 17:20:34 -0700

    5-printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main
+ * main - Entry point
  * printf: To print the exact same text that inside it
- * return
+ * return: Always 0 (Success)
  */
-int main(void)
+int main()
 {
 	printf("with proper grammar, but the outcome is a piece of art,");
-	return(0);
+	return (0);
 }
