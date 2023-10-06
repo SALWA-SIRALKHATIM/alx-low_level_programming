09989b3737a9ff79f2d1132880509ea221ce324f
SALWA-SIRALKHATIM <salwasiralkhatim@gmail.com>
2023-10-05 17:12:36 -0700

    44

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 /**
- * Short discription: The function is to print the exact same text that is wanted
+ * Short discription: To print the exact same text that is wanted
  * main: Entry point (the main fuction)
  * puts function:To print the exact text inside it
  * return: Will always return 0
  */
 int main(void)
 {
+	/*main: Entry point (the main fuction)*/
+	/*The function is to print the exact same text that is wanted*/
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	return (0); 
+	/*return: Will always return 0*/
 }
