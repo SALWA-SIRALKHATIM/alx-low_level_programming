e9bd38a8e12a7b89b4315bb6c320c47bee6e97d1
SALWA-SIRALKHATIM <salwasiralkhatim@gmail.com>
2023-10-06 17:39:12 -0700

    3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return:Always 1 (Success)
+ */
+int main(void)
+{
+	/* Declare a variable and initialize it with the first member of 
+	 * the range [a - z]
+	*/
+
+	char alphabet = 'a';
+
+	/* check to see if the current value of your variable is less than 
+	or the same as the last member (z) of the given range. While 
+	the value is less, go ahead and print the value
+	*/
+
+	while (alphabet <= 'z')
+	{
+	/* print the value of the variable */
+	putchar(alphabet);
+
+	/* increment the variable (ie: a becomes b)*/
+	alphabet++;
+	}
+	char alphabet = 'A';
+
+	/* check to see if the current value of your variable is less than
+	or the same as the last member (z) of the given range. While
+	the value is less, go ahead and print the value
+	*/
+
+	while (alphabet <= 'Z')
+	{
+	/* print the value of the variable */
+	putchar(alphabet);
+
+	/* increment the variable (ie: a becomes b) */
+	alphabet++;
+	}
+
+	putchar('\n');
+	return (0);
+}
+
