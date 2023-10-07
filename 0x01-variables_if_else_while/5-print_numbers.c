0955b9c04e741c72ab9a864b0020baaf370feced
SALWA-SIRALKHATIM <salwasiralkhatim@gmail.com>
2023-10-06 18:03:47 -0700

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return:Always 1 (Success)
+ */
+int main ()
+{
+	int num = 0;
+
+	while (num<10)
+	{
+		printf(num);
+		num++
+	}
+	return (0);
+}
