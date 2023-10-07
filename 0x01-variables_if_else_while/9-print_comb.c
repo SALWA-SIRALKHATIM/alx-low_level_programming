334d8d1900d6f8a411993a62714d9fe3ba4e4204
SALWA-SIRALKHATIM <salwasiralkhatim@gmail.com>
2023-10-06 18:42:27 -0700

    9. Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return:Always 1 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0, num < 10, num++)
+	{
+		putchar((num % 10) + '0');
+		if (num == 9)
+			continue;
+
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
