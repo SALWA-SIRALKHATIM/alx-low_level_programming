2883ef29a81a1a71e01f49bca183d710fa901641
SALWA-SIRALKHATIM <salwasiralkhatim@gmail.com>
2023-10-10 17:31:26 -0700

    1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,17 @@
+/**
+ * File: 1-alphabet.c
+ * Auth: Salwa
+ */
 #include "main.h"
 
 /**
- * main - prints the alphabet, in lowercase, followed by a new line.
- * Author: Salwa SirAlkhatim
+ * prints the alphabet, in lowercase, followed by a new line.
  */
 
 void print_alphabet(void);
 {
 	char ch;
+
 	for (ch = 'a'; ch <= 'z', ch++)
 		_putchar(ch);
 
