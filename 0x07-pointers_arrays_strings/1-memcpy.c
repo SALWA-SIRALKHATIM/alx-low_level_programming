89099c3e7940972325b335da9c2fddffa990fae5
SALWA-SIRALKHATIM <salwasiralkhatim@gmail.com>
2023-10-23 17:46:11 -0700

    task2

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+/*
+ * File: 1-memcpy.c
+ * Auth: Salwa
+ */
+
+#include "main.h"
+
+/**
+ * _memcpy - copies n bytes from memory area src to memory area dest
+ * @dest: area copied to
+ * @src: area copied
+ * @n: number of bytes
+ *
+ * Return: a pointer to the memory area dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+	return (dest);
+}
