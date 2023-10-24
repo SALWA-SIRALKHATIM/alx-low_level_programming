0b5807b94e93327b0583f831aeb3044c77172a14
SALWA-SIRALKHATIM <salwasiralkhatim@gmail.com>
2023-10-23 17:53:51 -0700

    task3

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+/*
+ * File: 2-strchr.c
+ * Auth: Salwa
+ */
+
+#include "main.h"
+#define NULL 0
+
+/**
+ * _strchr - finds the first occurrence of the character c in the string s,
+ * or NULL if the character is not found.
+ * @s: string to be checked
+ * @c: character to be looked for
+ *
+ * Return: pointer to c or NULL if character is not found
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s)
+		if (*s++ == c)
+			return (--s);
+	if (c == '\0')
+		return (s);
+	return (NULL);
+}
