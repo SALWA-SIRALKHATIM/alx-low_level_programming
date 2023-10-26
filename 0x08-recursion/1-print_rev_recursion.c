4c5087149d8d8dc6170d5f7cfc0b61bc5eb45fc4
SALWA-SIRALKHATIM <salwasiralkhatim@gmail.com>
2023-10-25 17:47:49 -0700

    task2

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -6,15 +6,15 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion: Prints a string in revers, followed by a new line.
+ * _print_rev_recursion - Prints a string in revers, followed by a new line.
  * @s: The string to be printed.
  */
 
 void _print_rev_recursion(char *s)
 {
-if (s[0] != '\0')
-{
-	 _print_rev_recursion(s + 1);
-	 _putchar(s[0]);
-}
+	if (s[0] != '\0')
+	{
+		 _print_rev_recursion(s + 1);
+		 _putchar(s[0]);
+	}
 }
