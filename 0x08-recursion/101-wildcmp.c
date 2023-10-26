67fc71371e4e152b58f9799047982a0fc35fc08c
SALWA-SIRALKHATIM <salwasiralkhatim@gmail.com>
2023-10-25 20:04:21 -0700

    task9

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,9 +1,11 @@
-/**
+/*
  * File: 101-wildcmp.c
  * Auth: Salwa
  */
- #include "main.h"
- int strlen_no_wilds(char *str);
+
+#include "main.h"
+
+int strlen_no_wilds(char *str);
 void iterate_wild(char **wildstr);
 char *postfix_match(char *str, char *postfix);
 int wildcmp(char *s1, char *s2);
