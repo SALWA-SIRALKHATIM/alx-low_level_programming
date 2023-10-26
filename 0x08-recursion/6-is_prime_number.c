85afb533fd5fe5ffad375aed8d115e4b764efc0b
SALWA-SIRALKHATIM <salwasiralkhatim@gmail.com>
2023-10-25 19:06:01 -0700

    task7

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -22,7 +22,7 @@ int is_divisible(int num, int div)
 		return (1);
 	else if (num % div == 0)
 		return (0);
-	else 
+	else
 		return (is_divisible(num, div + 1));
 }
 
@@ -37,6 +37,6 @@ int is_prime_number(int n)
 {
 	if (n <= 1)
 		return (0);
-	else 
+	else
 		return (is_divisible(n, 2));
 }
