8dc8339dff314b2108fdbbc8e37e59d47753418e
SALWA-SIRALKHATIM <salwasiralkhatim@gmail.com>
2023-10-25 17:44:03 -0700

    task2

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+/**
+ * File: 1-print_rev_recursion.c
+ * Auth: Salwa
+ */
+
+#include "main.h"
+
+/**
+ * _print_rev_recursion: Prints a string in revers, followed by a new line.
+ * @s: The string to be printed.
+ */
+
+void _print_rev_recursion(char *s)
+{
+if (s[0] != '\0')
+{
+	 _print_rev_recursion(s + 1);
+	 _pytchar(s[0]);
+}
+}
