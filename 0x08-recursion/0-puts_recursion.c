38a75d7a7d62668105cd8752a681962eb7c13f4b
SALWA-SIRALKHATIM <salwasiralkhatim@gmail.com>
2023-10-25 17:21:23 -0700

    task1

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+/**
+ * File: 0-puts_recursion.c
+ * Auth: Salwa
+ */
+#include "main.h"
+
+/**
+ * _puts_recursion - Write a function that prints a string, followed by a new line.
+ * @s: the string to be printed.
+ * Reaturn: nothing.
+ */
+
+void _puts_recursion(char *s){
+	if s[0] =! '\0'
+	{
+		_putchar(s[0]);
+		_puts_recursion(s+1);
+	}
+	else
+		_putchar('\n');
+}
