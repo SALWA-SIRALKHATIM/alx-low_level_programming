89d750199db5469d8598bcadf99262c9fc59d003
SALWA-SIRALKHATIM <salwasiralkhatim@gmail.com>
2023-10-25 17:56:26 -0700

    task3

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+/*
+ * File: 2-strlen_recursion.c
+ * Auth: Salwa
+ */
+
+#include "main.h"
+
+/**
+ * _strlen_recursion - Returns the length of a string.
+ * @s: The string to be measured.
+ *
+ * Return: The length of the string.
+ */
+int _strlen_recursion(char *s);
+{
+	if ( s[0] != '\0')
+		return (1 + _strlen_recursion(s + 1));
+}
